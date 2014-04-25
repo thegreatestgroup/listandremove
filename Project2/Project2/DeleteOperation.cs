c5e2d17e67e36d7cb68e6f44e41313c19105430e
Daniel Puckowski <puckowski.d@gmail.com>
2014-04-25 15:39:36 -0400

    Added preliminary update song.

---
+++ Project2/Project2/DeleteOperation.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finale
+{
+    class DeleteOperation
+    {
+        /* GENERIC SQL DELETE STATEMENT (more info ... http://www.w3schools.com/sql/sql_delete.asp)
+         *  DELETE FROM table_name
+         *  WHERE some_column=some_value;
+         */
+
+        /*
+         *  DELETE FROM tableName
+         *  WHERE columnName=valueToDelete;
+         */
+        public void deleteAttributeFromTableWithoutTransaction(string tableName, string columnName, string valueToDelete)
+        {
+            string deleteQuery = "DELETE FROM " + tableName;
+            deleteQuery +=      " WHERE " + columnName + "=" + valueToDelete + ";";
+
+            SQLConnection sqlConnection = new SQLConnection();
+
+            sqlConnection.execute(tableName, deleteQuery);
+        }
+
+        /*
+         *  DELETE FROM tableName
+         *  WHERE columnName=valueToDelete;
+         */
+        public void deleteAttributeFromTable(string tableName, string columnName, string valueToDelete)
+        {
+            string deleteQuery = "DECLARE @TranName VARCHAR(20);" +
+                                " SELECT @TranName = 'DeleteTransaction';" +
+                                " BEGIN TRANSACTION @TranName;" +
+                                " WITH MARK N'Deleting an attribute.';";
+            deleteQuery +=      " DELETE FROM " + tableName;
+            deleteQuery +=      " WHERE " + columnName + "=" + valueToDelete + ";";
+            deleteQuery +=      " COMMIT TRANSACTION @TranName;" +
+                                " GO";
+
+            SQLConnection sqlConnection = new SQLConnection();
+
+            sqlConnection.execute(tableName, deleteQuery);
+        }
+
+        /*
+         *  DELETE FROM tableName
+         *  sqlWhereClause (where sqlWhereClause is a string something like "WHERE CustomerName='Alfreds Futterkiste' AND ContactName='Maria Anders';")
+         *  for complex delete operations.
+         */
+        public void deleteAttributeFromTableComplex(string tableName, string columnName, string sqlWhereClause)
+        {
+            string deleteQuery = "DECLARE @TranName VARCHAR(20);" +
+                                " SELECT @TranName = 'DeleteTransactionComplex';" +
+                                " BEGIN TRANSACTION @TranName;" +
+                                " WITH MARK N'Deleting an attribute.';";
+            deleteQuery +=      " DELETE FROM " + tableName;
+            deleteQuery +=      " " + sqlWhereClause;
+            deleteQuery +=      " COMMIT TRANSACTION @TranName;" +
+                                " GO";
+
+            SQLConnection sqlConnection = new SQLConnection();
+
+            sqlConnection.execute(tableName, deleteQuery);
+        }
+
+        /*
+         *  DELETE FROM tableName
+         *  sqlWhereClause (where sqlWhereClause is a string something like "WHERE CustomerName='Alfreds Futterkiste' AND ContactName='Maria Anders';")
+         *  for complex delete operations.
+         */
+        public void deleteAttributeFromTableComplexWithRollback(string tableName, string columnName, string sqlWhereClause)
+        {
+            string deleteQuery = "BEGIN TRY " + 
+                                " DECLARE @TranName VARCHAR(20);" +
+                                " SELECT @TranName = 'DeleteTransactionComplex';" +
+                                " BEGIN TRANSACTION @TranName;" +
+                                " WITH MARK N'Deleting an attribute.';";
+            deleteQuery +=      " DELETE FROM " + tableName;
+            deleteQuery +=      " " + sqlWhereClause;
+            deleteQuery +=      " COMMIT TRANSACTION @TranName;" +
+                                " GO" +
+                                " END TRY";
+            deleteQuery +=      " BEGIN CATCH" +
+                                " IF @@TRANCOUNT > 0" +
+                                " ROLLBACK" +
+                                " END CATCH";
+
+            SQLConnection sqlConnection = new SQLConnection();
+
+            sqlConnection.execute(tableName, deleteQuery);
+        }
+    }
+}
---
+++ Project2/Project2/Search.cs
@@ -0,0 +1,230 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace Finale
+{
+    class Search
+    {
+        public System.Data.DataTable SearchFor(Song tempSong)
+        {
+            //
+            return new DataTable(); 
+        }
+
+        public bool isSongIdExist(string songId)
+        {
+            string query = "SELECT Song.ID" +
+                          " FROM Song WHERE" +
+                          " Song.ID LIKE " + songId + ";";
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isSongExist(string songName)
+        {
+            string query = "SELECT Song.ID" +
+                          " FROM Song WHERE" +
+                          " Song.Name LIKE " + songName + ";";
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isAlbumIdExist(string albumId)
+        {
+            string query = "SELECT Album.ID" +
+                          " FROM Album WHERE" +
+                          " Album.ID LIKE " + albumId + ";";
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isAlbumExist(string albumName)
+        {
+            string query = "SELECT Album.ID" +
+                          " FROM Album WHERE" +
+                          " Album.Name LIKE " + albumName + ";";
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isComposerIdExist(string composerId)
+        {
+            string query = "SELECT Composer.ID" +
+                          " FROM Composer WHERE" +
+                          " Composer.ID LIKE " + composerId + ";";
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isComposerExist(string composerName)
+        {
+            string query = "SELECT Composer.ID" +
+                          " FROM Composer WHERE" +
+                          " Composer.Name LIKE " + composerName + ";";
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isGenreIdExist(string genreId)
+        {
+            string query = "SELECT Genre.ID" +
+                          " FROM Genre WHERE" +
+                          " Genre.ID LIKE " + genreId + ";";
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isGenreExist(string genreType)
+        {
+            string query = "SELECT Genre.ID" +
+                          " FROM Genre WHERE" +
+                          " Genre.Type LIKE " + genreType + ";"; // Genre.Type ? 
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isPriceIdExist(string priceValueId)
+        {
+            string query = "SELECT PriceValue.ID" +
+                          " FROM PriceValue WHERE" +
+                          " PriceValue.ID LIKE " + priceValueId + ";";
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isPriceExist(string price)
+        {
+            string query = "SELECT PriceValue.ID" +
+                          " FROM PriceValue WHERE" +
+                          " PriceValue.Price LIKE " + price + ";";
+
+            System.Data.DataTable table;
+
+            SQLConnection sqlConnection = new SQLConnection();
+            table = sqlConnection.execute("SearchResults", query);
+
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
